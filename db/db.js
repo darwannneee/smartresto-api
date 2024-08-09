bd7b37570349c28a5702a3efc89b630edf9f5ca0
darwannneee <darwanchannel1@gmail.com>
2024-08-10 03:20:21 +0700

    change new pass for db

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 const mysql2 = require('mysql2');
 
 // Use environment variables for database configuration
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
     dialectModule: mysql2,
